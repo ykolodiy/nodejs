6a4b1093b98838d07770408f66d6678b77007cda
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2017-10-30 13:48:00 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,41 +1,31 @@
-//import modules    
-var http = require('http');  
-    var fs = require('fs');  
-    var url = require('url');  
-      
-    // Create a server  
-    http.createServer( function (request, response) {    
-       // Parse the request containing file name  
-       var pathname = url.parse(request.url).pathname;  
-         
-       // Print the name of the file for which request is made.  
-       console.log("Request for " + pathname + " received.");  
-         
-         
-       // Read the requested file content from file system  
-       fs.readFile(pathname.substr(1), function (err, data) 
-       {  
-          if (err) {  
-             console.log(err);  
-             // HTTP Status: 404 : NOT FOUND  
-             // Content Type: text/plain  
-             response.writeHead(404, {'Content-Type': 'text/html'});  
-          }else{      
-             //Page found       
-             // HTTP Status: 200 : OK  
-             // Content Type: text/plain  
-             response.writeHead(200, {'Content-Type': 'text/html'});      
-               
-             // Write the content of the file to response body  
-             response.write(data.toString());         
-          }  
-          // Send the response body   
-          response.end();  
-       });  
+var http = require("http");
+var fs = require("fs");
+var util = require("util");
+
+var content;
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   content = data;
+   console.log(data.toString());
+});
 
 
 
 
-    }).listen(8081);  
-    // Console will print the message  
-    console.log('Server running at http://127.0.0.1:8081/');  
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as data from text file input.txt
+
+
+
+
+response.end(content + "ty suka");
+}).listen(8282);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8282/');
